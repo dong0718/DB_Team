1855d43b5d001eedbef7ed1d550799767a3dbc6e
dong0718 <93818863+dong0718@users.noreply.github.com>
2021-11-06 18:09:22 +0900

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,11 +29,48 @@ public class Main {
                 }
                 break;
             case 2:
+
                 System.out.println("원하는 소분류 기능 선택");
+                System.out.println("1.전체조회  2.학년별조회  3.교과목추가  4.교과목명변경");
+                SubjectDAO subjectDAO = new SubjectDAO(MybatisConnectionFactory.getSqlSessionFactory());
                 int secondmenu=sc.nextInt();
                 switch (secondmenu)
                 {
+                    case 1:
+                        List<SubjectDTO> subjectDTOS1 = subjectDAO.select_all();
+                        System.out.println("< 전체 교과목 조회 >");
+                        for(SubjectDTO dto:subjectDTOS1){
+                            System.out.println(dto.toString());
+                        }
+                        System.out.println();
+                        break;
+                    case 2:
+                        int grade = 2; //조회할 학년 입력
+                        List<SubjectDTO> subjectDTOS2 = subjectDAO.select_by_grade(grade);
+                        System.out.println("< 해당 학년 교과목 조회 (선택된 학년 : " + grade + "학년) >");
+                        for(SubjectDTO dto:subjectDTOS2){
+                            System.out.println(dto.toString());
+                        }
+                        System.out.println();
+                        break;
 
+                    case 3:
+                        SubjectDTO subjectDTOS3 = new SubjectDTO();
+                        subjectDTOS3.setSubcode("qwer20");  //추가할 과목의 과목코드
+                        subjectDTOS3.setSubname("testsub"); //추가할 과목의 과목명
+                        subjectDTOS3.setSubinfo("testinfo"); //추가할 과목의 과목정보
+                        subjectDTOS3.setSubgrade(2); //추가할 과목의 학년
+                        System.out.println("추가됨 : " + subjectDTOS3.toString());
+                        subjectDAO.insert_subject(subjectDTOS3);
+                        break;
+
+                    case 4:
+                        SubjectDTO subjectDTOS4 = new SubjectDTO();
+                        subjectDTOS4.setSubcode("qwer2");  //과목명을 변경할 과목의 과목코드
+                        subjectDTOS4.setSubname("changed1");  //변경할 과목명
+                        System.out.println("과목명 변경됨 : " + subjectDTOS4.toString());
+                        subjectDAO.update_subname(subjectDTOS4);
+                        break;
                 }
                 break;
             case 3:
